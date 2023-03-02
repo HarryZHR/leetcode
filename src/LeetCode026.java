cadd4a66375f4b03c634ad4ad2b0273f48e3f8bc
Harry <609896631@qq.com>
2023-03-02 22:08:50 +0800

    LeetCode026

---
+++ src/LeetCode026.java
@@ -0,0 +1,89 @@
+/**
+ * 给你一个 升序排列 的数组 nums ，请你 原地 删除重复出现的元素，使每个元素 只出现一次 ，返回删除后数组的新长度。元素的 相对顺序 应该保持 一致 。
+ *
+ * 由于在某些语言中不能改变数组的长度，所以必须将结果放在数组nums的第一部分。更规范地说，如果在删除重复项之后有 k 个元素，那么nums的前 k 个元素应该保存最终结果。
+ *
+ * 将最终结果插入 nums 的前 k 个位置后返回 k 。
+ *
+ * 不要使用额外的空间，你必须在 原地 修改输入数组 并在使用 O(1) 额外空间的条件下完成。
+ *
+ * 判题标准:
+ *
+ * 系统会用下面的代码来测试你的题解:
+ *
+ * int[] nums = [...]; // 输入数组
+ * int[] expectedNums = [...]; // 长度正确的期望答案
+ *
+ * int k = removeDuplicates(nums); // 调用
+ *
+ * assert k == expectedNums.length;
+ * for (int i = 0; i < k; i++) {
+ *     assert nums[i] == expectedNums[i];
+ * }
+ * 如果所有断言都通过，那么您的题解将被 通过。
+ *
+ *示例 1：
+ *
+ * 输入：nums = [1,1,2]
+ * 输出：2, nums = [1,2,_]
+ * 解释：函数应该返回新的长度 2 ，并且原数组 nums 的前两个元素被修改为 1, 2 。不需要考虑数组中超出新长度后面的元素。
+ *
+ * 示例 2：
+ *
+ * 输入：nums = [0,0,1,1,1,2,2,3,3,4]
+ * 输出：5, nums = [0,1,2,3,4]
+ * 解释：函数应该返回新的长度 5 ， 并且原数组 nums 的前五个元素被修改为 0, 1, 2, 3, 4 。不需要考虑数组中超出新长度后面的元素。
+ *
+ *
+ * 提示：
+ *
+ * 1 <= nums.length <= 3 * 104
+ * -104 <= nums[i] <= 104
+ * nums 已按 升序 排列
+ */
+
+public class LeetCode026 {
+
+    /**
+     * 类似于暴力解法 先把位置占住了 再重新排序
+     * @param nums
+     * @return
+     */
+    public int removeDuplicates(int[] nums) {
+        int[] indexArr = new int[20000];
+        for (int i = 0; i < nums.length; i++) {
+            indexArr[nums[i] + 10000] = 1;
+        }
+        int count = 0;
+        for (int i = 0; i < indexArr.length; i++) {
+            if (indexArr[i] == 1) {
+                nums[count] = i - 10000;
+                count ++;
+            }
+        }
+        return count;
+    }
+
+
+    public int removeDuplicates2(int[] nums) {
+        int i = 0;
+        int j = 1;
+        while (j < nums.length) {
+            if (nums[i] != nums[j]) {
+                nums[i + 1] = nums[j];
+                i++;
+            }
+            j++;
+        }
+        return i + 1;
+    }
+
+    public static void main(String[] args) {
+        LeetCode026 leetCode026 = new LeetCode026();
+        int[] a = {1,1,2};
+//        int[] a = {0,0,1,1,1,2,2,3,3,4};
+        System.out.println(leetCode026.removeDuplicates2(a));
+    }
+
+
+}
